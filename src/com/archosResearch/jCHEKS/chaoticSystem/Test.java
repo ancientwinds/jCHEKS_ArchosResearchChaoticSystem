de51aaa7f532cb66baa5024cffcd4f212c4d719b
frgagnon <frgagnon@ste-foy.qc.ca>
2015-05-19 22:28:33 -0400

    Project initialization

---
+++ src/com/archosResearch/jCHEKS/chaoticSystem/Test.java
@@ -0,0 +1,14 @@
+package com.archosResearch.jCHEKS.chaoticSystem;
+
+import com.archosResearch.jCHEKS.concept.encrypter.*;
+import com.archosResearch.jCHEKS.concept.chaoticSystem.*;
+
+/**
+ *
+ * @author fgagnon
+ */
+public class Test extends AbstractChaoticSystem{
+
+
+
+}
