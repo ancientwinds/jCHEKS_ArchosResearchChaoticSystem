60f613f2f1e5f6abf4bf2d325d5f3b14332d93b7
LepageT <thomas.lepage@hotmail.ca>
2015-06-18 12:36:40 -0400

    Fix tests.

--- test/ChaoticSystemTest.java
+++ test/ChaoticSystemTest.java
@@ -9,28 +9,22 @@ import com.archosResearch.jCHEKS.chaoticSystem.ChaoticSystemMock;
  */
 public class ChaoticSystemTest {
     @Test
-    public void ChaoticSystemMockTest() {
+    public void ChaoticSystemMockTest() throws Exception{
         ChaoticSystemMock systemMock1 = new ChaoticSystemMock();
-        try {
-            systemMock1.Generate(128);
-        } catch (Exception e) {
-            //
-        }
         ChaoticSystemMock systemMock2 = new ChaoticSystemMock();
+        
         systemMock2.Deserialize(systemMock1.Serialize());
+        
         assertTrue("Serialization and Deserialization of mock system", systemMock1.Serialize().equals(systemMock2.Serialize()));
         }
     
     @Test
-    public void ChaoticSystemTest() {
-        ChaoticSystem system1 = new ChaoticSystem();
-        try {
-            system1.Generate(128);
-        } catch (Exception e) {
-            //
-        }
-        ChaoticSystem system2 = new ChaoticSystem();
+    public void ChaoticSystemTest() throws Exception {
+        ChaoticSystem system1 = new ChaoticSystem(java.util.UUID.randomUUID().toString(), 128);
+        ChaoticSystem system2 = new ChaoticSystem(java.util.UUID.randomUUID().toString(), 128);
+        
         system2.Deserialize(system1.Serialize());
+        
         assertTrue("Serialization and Deserialization of system", system1.Serialize().equals(system2.Serialize()));
     }
 }
