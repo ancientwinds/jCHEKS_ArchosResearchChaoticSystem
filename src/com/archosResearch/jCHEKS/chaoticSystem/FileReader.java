bb7891e2836a5063004941fe36f831adb4f75e15
LepageT <thomas.lepage@hotmail.ca>
2015-06-22 11:14:42 -0400

    Small fix when saving and reading from the system folder.

--- src/com/archosResearch/jCHEKS/chaoticSystem/FileReader.java
+++ src/com/archosResearch/jCHEKS/chaoticSystem/FileReader.java
@@ -26,35 +26,39 @@ import org.w3c.dom.Document;
  */
 public class FileReader {
     
+    private static final String chaoticSystemDir = "system/";
+    
     public ChaoticSystem readChaoticSystem(String fileName) throws Exception {
-        File dir = new File("system/");
+        File dir = new File(chaoticSystemDir);
         dir.mkdirs();
+        String fileToSave = chaoticSystemDir + fileName;
         ChaoticSystem system = new ChaoticSystem(128);
-        String extension = this.getFileExtension(fileName);
+        String extension = this.getFileExtension(fileToSave);
         if(extension.equals("xml")) {            
-            system.deserializeXML(new File("system/" + fileName));
+            system.deserializeXML(new File(fileToSave));
             return system;
         } else if(extension.equals("sre")) {
-            system.Deserialize(readFile("system/" + fileName, null));
+            system.Deserialize(readFile(fileToSave, null));
             return system;  
         }        
         throw new Exception("File not compatible");
     }
     
     public void saveChaoticSystem(String fileName, ChaoticSystem system) throws Exception {
-        File dir = new File("system/");
+        File dir = new File(chaoticSystemDir);
         dir.mkdirs();
-        String extension = this.getFileExtension(fileName);
+        String fileToSave = chaoticSystemDir + fileName;
+        String extension = this.getFileExtension(fileToSave);
         if(extension.equals("xml")) {
             Document doc = system.serializeXML();
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("system/" + fileName));
+            StreamResult result = new StreamResult(new File(fileToSave));
             transformer.transform(source, result);
         } else if(extension.equals("sre")) {
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream("system/" + fileName), "utf-8"))) {
+                new FileOutputStream(fileToSave), "utf-8"))) {
                 writer.write(system.Serialize());         
             }
         }
