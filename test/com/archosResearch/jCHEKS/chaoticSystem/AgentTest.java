4ccebbb83601ba311da0dfdd033a850eda79b098
LepageT <thomas.lepage@hotmail.ca>
2015-07-13 10:52:45 -0400

    Fix tests.

--- test/com/archosResearch/jCHEKS/chaoticSystem/AgentTest.java
+++ test/com/archosResearch/jCHEKS/chaoticSystem/AgentTest.java
@@ -15,21 +15,14 @@ public class AgentTest {
     
     @Test
     public void agent_keyPart_should_not_change_if_there_is_no_pending_impacts() {
-        try {
-            Range impactRange = new Range(0, 32);
-            Range keyPartRange = new Range(-128, 127);
-            Range delayRange = new Range(0 ,4);
-            
-            Agent agent = new Agent(1, impactRange, keyPartRange, delayRange, 0, 0, new Random("test".hashCode()));
-            int keyBeforeEvolve = agent.getKeyPart();
-            agent.evolve(0, 0);
-            assertEquals(keyBeforeEvolve, (int) agent.getKeyPart());
-            
-            Rule rule = new Rule(1, impactRange, delayRange, new Random("test".hashCode()));
-            System.out.println(rule.serializeXml());
-        } catch (XMLSerializationException ex) {
-            Logger.getLogger(AgentTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Range impactRange = new Range(0, 32);
+        Range keyPartRange = new Range(-128, 127);
+        Range delayRange = new Range(0 ,4);
+
+        Agent agent = new Agent(1, impactRange, keyPartRange, delayRange, 0, 0, new Random("test".hashCode()));
+        int keyBeforeEvolve = agent.getKeyPart();
+        agent.evolve(0, 0);
+        assertEquals(keyBeforeEvolve, (int) agent.getKeyPart());
     }
     
 }
